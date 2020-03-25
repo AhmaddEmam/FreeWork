4cb724e8e77fb471e8c1ddb21f8030fc0ec0c9ef
Ahmad Emam <ahmaddemam@gmail.com>
2020-03-25 23:35:54 +0200

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,53 @@
+window.alert('This Site is still under building..')
+
+$('document').ready(function () {
+    'use strict';
+    var typed = new Typed('#typed', {
+            strings: ["START YOUR CAREER"],
+            typeSpeed: 100
+        });
+    var typed = new Typed('#typedd', {
+            strings: ["HIRE EMPLOYEE"],
+            startDelay:3000,
+            typeSpeed: 100
+        });
+    var typed = new Typed('#typeddd', {
+            strings: ["FIND WORK"],
+            startDelay: 6000,
+            typeSpeed: 100
+        });
+});
+
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    'use strict';
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
+
+
+
+
+
+
+
+
+
+
